7d9f6f1495b60071c215fed04f0a052ef763bf55
Jing Zhou <jingzh@ordersell-lm.corp.yahoo.com>
2015-01-18 21:45:19 -0800

    Initial Commit

---
+++ tipcalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by Jing Zhou on 1/18/15.
+//  Copyright (c) 2015 Jingzh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tipcalculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  tipcalculator
+//
+//  Created by Jing Zhou on 1/18/15.
+//  Copyright (c) 2015 Jingzh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
