35c8283d88d8ede405defd1148401d296504cc1a
Jing Zhou <jingzh@ordersell-lm.corp.yahoo.com>
2015-01-19 22:00:45 -0800

    first commit

---
+++ tipcalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by Jing Zhou on 1/19/15.
+//  Copyright (c) 2015 Jingzh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
